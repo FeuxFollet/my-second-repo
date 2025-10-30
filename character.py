5028c7eb38d5fcc0bf7d014890db8602d6f96bfd
Feux Follets <phirawitsommai@gmail.com>
2025-10-30 21:09:56 +0700

    Modified Version

--- character.py
+++ character.py
@@ -1,15 +1,21 @@
+import random
+
 class Character:
     def __init__(self, name, hp, attack_power):
         self.name = name
         self.hp = hp
         self.attack_power = attack_power
-
-    def attack(self, other):
-        other.hp -= self.attack_power
-        print(f"{self.name} attacks {other.name} for {self.attack_power} damage!")
-
+    
+    def attack(self, other): 
+        ran_int = random.randint(30, 90)
+        ran_perc = (ran_int / 100) 
+        an_pwr = round(self.attack_power * ran_perc, 4)
+        other.hp = round(other.hp - ran_pwr, 4)
+        print(f"{self.name} attacks {other.name} for {ran_pwr} damage!")
+        
     def is_alive(self):
         return self.hp > 0
-
-    def __str__(self):
+    
+    def __str__(self): 
         return f"{self.name}: {self.hp} HP"
+    
--- main.py
+++ main.py
@@ -3,24 +3,28 @@ from character import Character
 def main():
     hero = Character("Knight", 30, 5)
     enemy = Character("Goblin", 20, 3)
+    enemy2 = Character("Zombie", 15, 2)
 
     print(hero)
     print(enemy)
-    print()
+    print(enemy2)
 
-    while hero.is_alive() and enemy.is_alive():
-        hero.attack(enemy)
+    while hero.is_alive() and (enemy.is_alive() or enemy2.is_alive()):
         if enemy.is_alive():
+            hero.attack(enemy)
             enemy.attack(hero)
+        if enemy2.is_alive():
+            hero.attack(enemy2)
+            enemy2.attack(hero)
         print(hero)
         print(enemy)
-        print()
+        print(enemy2)
 
     print("Battle over!")
     if hero.is_alive():
         print(f"{hero.name} wins!")
     else:
-        print(f"{enemy.name} wins!")
+        print(f"{enemy.name} & {enemy2.name} wins!")
 
 if __name__ == "__main__":
     main()
